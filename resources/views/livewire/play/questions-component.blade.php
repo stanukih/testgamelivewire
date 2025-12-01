9d4fa952e2dac3ffef827813c2431330fa5a2cf9
Stanislav Ivkin <stanukih@gmail.com>
2025-12-01 11:56:21 +0200

    questions component remove json

--- resources/views/livewire/play/questions-component.blade.php
+++ resources/views/livewire/play/questions-component.blade.php
@@ -2,5 +2,5 @@
     @if ($questions->count()>0)
         <livewire:play.question-component :question="$questions[0]" :key="'$question_title' .$questions[0]->id"></livewire:play.question-component>
     @endif
-    <b>@json($this)</b>
+    
 </div>
